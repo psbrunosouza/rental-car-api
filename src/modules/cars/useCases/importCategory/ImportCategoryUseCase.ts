6f586cfbc88a1c50b250a7cd9c6f69234bb66071
psbrunosouza <psbrunosouza@gmail.com>
2022-05-28 22:22:05 -0300

    feat: start swagger documentation

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,26 +1,48 @@
 import {CategoryRepository} from "../../repositories/CategoryRepository";
 import {parse} from "csv-parse";
 import fs from "fs";
+import {ICategoryDTO} from "../../dtos/ICategoryDTO";
 
 export class ImportCategoryUseCase {
 
   constructor(private categoryRepository: CategoryRepository) {
   }
 
-  execute(file: Express.Multer.File | undefined): void {
+  private loadCategories(file: Express.Multer.File | undefined): Promise<ICategoryDTO[]> {
 
-    if (!file) {
-      throw new Error("File is undefined")
-    }
+    return new Promise((resolve, reject) => {
+      const categories: ICategoryDTO[] = [];
 
-    const stream = fs.createReadStream(file.path);
+      if (!file) {
+        throw new Error("File is undefined")
+      }
 
-    const parser = parse();
+      const stream = fs.createReadStream(file.path);
 
-    stream.pipe(parser);
+      const parser = parse();
 
-    parser.on("data", async (line) => {
-      console.log(line)
+      stream.pipe(parser);
+
+      parser.on("data", async (line) => {
+        const [name, description] = line;
+
+        categories.push({name, description});
+      }).on("end", () => {
+        return resolve(categories)
+      });
     })
+  }
+
+  async execute(file: Express.Multer.File | undefined): Promise<void> {
+    const categories = await this.loadCategories(file);
+
+    categories.map((category) => {
+      const categoryExists = this.categoryRepository.findByName(category.name);
+
+        if(!categoryExists){
+          this.categoryRepository.create(category);
+        }
+      }
+    )
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import {routes} from "./routes";
+import swaggerUI from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use(routes);
 
